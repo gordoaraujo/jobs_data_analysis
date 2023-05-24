4beddc3c3bb07c7bdbbb371fd654e63925ab70e2
Jose Araujo <jcaraujoa@gmail.com>
2023-05-24 18:41:30 +0200

    Adds new files and analysis

--- src/dictionaries_data.py
+++ src/dictionaries_data.py
@@ -1,34 +1,34 @@
-# Adapted from https://github.com/lukebarousse/Data_Analyst_Streamlit_App_V1/blob/main/01_%F0%9F%9B%A0%EF%B8%8F_Skills.py
-keywords_skills = {
-    'airflow': 'Airflow', 'alteryx': 'Alteryx', 'aspnet': 'ASP.NET', 'atlassian': 'Atlassian', 
-    'excel': 'Excel', 'powerbi': 'Power BI', 'tableau': 'Tableau', 'srss': 'SRSS', 'word': 'Word', 
-    'unix': 'Unix', 'vue': 'Vue', 'jquery': 'jQuery', 'linuxunix': 'Linux / Unix', 'seaborn': 'Seaborn', 
-    'microstrategy': 'MicroStrategy', 'spss': 'SPSS', 'visio': 'Visio', 'gdpr': 'GDPR', 'ssrs': 'SSRS', 
-    'spreadsheet': 'Spreadsheet', 'aws': 'AWS', 'hadoop': 'Hadoop', 'ssis': 'SSIS', 'linux': 'Linux', 
-    'sap': 'SAP', 'powerpoint': 'PowerPoint', 'sharepoint': 'SharePoint', 'redshift': 'Redshift', 
-    'snowflake': 'Snowflake', 'qlik': 'Qlik', 'cognos': 'Cognos', 'pandas': 'Pandas', 'spark': 'Spark', 'outlook': 'Outlook'
-}
+# # Adapted from https://github.com/lukebarousse/Data_Analyst_Streamlit_App_V1/blob/main/01_%F0%9F%9B%A0%EF%B8%8F_Skills.py
+# keywords_skills = {
+#     'airflow': 'Airflow', 'alteryx': 'Alteryx', 'aspnet': 'ASP.NET', 'atlassian': 'Atlassian', 
+#     'excel': 'Excel', 'powerbi': 'Power BI', 'tableau': 'Tableau', 'srss': 'SRSS', 'word': 'Word', 
+#     'unix': 'Unix', 'vue': 'Vue', 'jquery': 'jQuery', 'linuxunix': 'Linux / Unix', 'seaborn': 'Seaborn', 
+#     'microstrategy': 'MicroStrategy', 'spss': 'SPSS', 'visio': 'Visio', 'gdpr': 'GDPR', 'ssrs': 'SSRS', 
+#     'spreadsheet': 'Spreadsheet', 'aws': 'AWS', 'hadoop': 'Hadoop', 'ssis': 'SSIS', 'linux': 'Linux', 
+#     'sap': 'SAP', 'powerpoint': 'PowerPoint', 'sharepoint': 'SharePoint', 'redshift': 'Redshift', 
+#     'snowflake': 'Snowflake', 'qlik': 'Qlik', 'cognos': 'Cognos', 'pandas': 'Pandas', 'spark': 'Spark', 'outlook': 'Outlook'
+# }
 
-# Adapted from https://github.com/lukebarousse/Data_Analyst_Streamlit_App_V1/blob/main/01_%F0%9F%9B%A0%EF%B8%8F_Skills.py
-keywords_programming = {
-    'sql' : 'SQL', 'python' : 'Python', 'r' : 'R', 'c':'C', 'c#':'C#', 'javascript' : 'JavaScript', 'js':'JS', 'java':'Java', 
-    'scala':'Scala', 'sas' : 'SAS', 'matlab': 'MATLAB', 'c++' : 'C++', 'perl' : 'Perl','go' : 'Go',
-    'typescript' : 'TypeScript','bash':'Bash','html' : 'HTML','css' : 'CSS','php' : 'PHP','powershell' : 'Powershell',
-    'rust' : 'Rust', 'kotlin' : 'Kotlin','ruby' : 'Ruby','dart' : 'Dart','assembly' :'Assembly',
-    'swift' : 'Swift','vba' : 'VBA','lua' : 'Lua','groovy' : 'Groovy','delphi' : 'Delphi','objectivec' : 'Objective-C',
-    'haskell' : 'Haskell','elixir' : 'Elixir','julia' : 'Julia','clojure': 'Clojure','solidity' : 'Solidity',
-    'lisp' : 'Lisp','f#':'F#','fortran' : 'Fortran','erlang' : 'Erlang','apl' : 'APL','cobol' : 'COBOL',
-    'ocaml': 'OCaml','crystal':'Crystal','javascripttypescript' : 'JavaScript / TypeScript','golang':'Golang',
-    'nosql': 'NoSQL', 'mongodb' : 'MongoDB','tsql' :'Transact-SQL','vba' : 'Visual Basic',
-    'pascal':'Pascal', 'mongo' : 'Mongo', 'plsql' : 'PL/SQL','sass' :'SASS', 'vbnet' : 'VB.NET','mssql' : 'MSSQL'
-}
+# # Adapted from https://github.com/lukebarousse/Data_Analyst_Streamlit_App_V1/blob/main/01_%F0%9F%9B%A0%EF%B8%8F_Skills.py
+# keywords_programming = {
+#     'sql' : 'SQL', 'python' : 'Python', 'r' : 'R', 'c':'C', 'c#':'C#', 'javascript' : 'JavaScript', 'js':'JS', 'java':'Java', 
+#     'scala':'Scala', 'sas' : 'SAS', 'matlab': 'MATLAB', 'c++' : 'C++', 'perl' : 'Perl','go' : 'Go',
+#     'typescript' : 'TypeScript','bash':'Bash','html' : 'HTML','css' : 'CSS','php' : 'PHP','powershell' : 'Powershell',
+#     'rust' : 'Rust', 'kotlin' : 'Kotlin','ruby' : 'Ruby','dart' : 'Dart','assembly' :'Assembly',
+#     'swift' : 'Swift','vba' : 'VBA','lua' : 'Lua','groovy' : 'Groovy','delphi' : 'Delphi','objectivec' : 'Objective-C',
+#     'haskell' : 'Haskell','elixir' : 'Elixir','julia' : 'Julia','clojure': 'Clojure','solidity' : 'Solidity',
+#     'lisp' : 'Lisp','f#':'F#','fortran' : 'Fortran','erlang' : 'Erlang','apl' : 'APL','cobol' : 'COBOL',
+#     'ocaml': 'OCaml','crystal':'Crystal','javascripttypescript' : 'JavaScript / TypeScript','golang':'Golang',
+#     'nosql': 'NoSQL', 'mongodb' : 'MongoDB','tsql' :'Transact-SQL','vba' : 'Visual Basic',
+#     'pascal':'Pascal', 'mongo' : 'Mongo', 'plsql' : 'PL/SQL','sass' :'SASS', 'vbnet' : 'VB.NET','mssql' : 'MSSQL'
+# }
 
-keywords_python = {'tensorflow': 'TensorFlow', 'numpy': 'NumPy', 'scipy': 'SciPy',
- 'pandas': 'Pandas', 'matplotlib': 'Matplotlib', 'keras': 'Keras', 'statsmodels': 'Statsmodels', 'scikitlearn': 'SciKit-Learn', 
- 'pytorch': 'PyTorch', 'pycaret': 'PyCaret', 'scrapy': 'Scrapy', 'beautifulsoup': 'BeautifulSoup', 'xgboost': 'XGBoost', 
- 'lightgbm': 'LightGBM', 'eli5': 'ELI5', 'theano': 'Theano', 'nupic': 'NuPIC', 'ramp': 'Ramp', 'pipenv': 'Pipenv',
- 'bob': 'Bob', 'pybrain': 'PyBrain', 'caffe2': 'Caffe2', 'chainer': 'Chainer', 'seaborn': 'Seaborn', 'bokeh': 'Bokeh', 
- 'plotly': 'Plotly', 'pydot': 'pydot'}
+# keywords_python = {'tensorflow': 'TensorFlow', 'numpy': 'NumPy', 'scipy': 'SciPy',
+#  'pandas': 'Pandas', 'matplotlib': 'Matplotlib', 'keras': 'Keras', 'statsmodels': 'Statsmodels', 'scikitlearn': 'SciKit-Learn', 
+#  'pytorch': 'PyTorch', 'pycaret': 'PyCaret', 'scrapy': 'Scrapy', 'beautifulsoup': 'BeautifulSoup', 'xgboost': 'XGBoost', 
+#  'lightgbm': 'LightGBM', 'eli5': 'ELI5', 'theano': 'Theano', 'nupic': 'NuPIC', 'ramp': 'Ramp', 'pipenv': 'Pipenv',
+#  'bob': 'Bob', 'pybrain': 'PyBrain', 'caffe2': 'Caffe2', 'chainer': 'Chainer', 'seaborn': 'Seaborn', 'bokeh': 'Bokeh', 
+#  'plotly': 'Plotly', 'pydot': 'pydot'}
 
-def load_keywords():
-    return keywords_skills, keywords_programming, keywords_python
+# def load_keywords():
+#     return keywords_skills, keywords_programming, keywords_python
